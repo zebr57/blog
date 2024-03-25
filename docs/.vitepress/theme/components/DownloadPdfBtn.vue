07491cafc9bfabc0cf05a2d7f0cc91d0d872f761
沈林强 <12514942+shen-linqiang@user.noreply.gitee.com>
2024-03-25 22:11:06 +0800

    feat: md中 vue sfc 的使用

---
+++ docs/.vitepress/theme/components/DownloadPdfBtn.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="download-btn" @click="handleDownload">下载PDF</button>
+</template>
+
+<script setup>
+import pdf from "../../../public/assets/沈林强简历.pdf";
+const handleDownload = () => {
+  // 打开新窗口并展示PDF
+  window.open(pdf);
+};
+</script>
+
+<style>
+.download-btn {
+  color: #666;
+}
+</style>
